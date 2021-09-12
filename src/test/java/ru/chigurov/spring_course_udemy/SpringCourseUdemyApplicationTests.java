6557a87911473977eba085fd782327a3164fbf3c
MikHail854 <vip.moneta95@mail.ru>
2021-09-12 22:07:11 +0700

    initialization

---
+++ src/test/java/ru/chigurov/spring_course_udemy/SpringCourseUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chigurov.spring_course_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
